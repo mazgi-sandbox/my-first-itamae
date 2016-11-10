d3a303ba3a75ab7569cfd4874a371dc8cbcdd87b
Hidenori MATSUKI <MATSUKI.Hidenori@gmail.com>
2016-11-10 18:28:55 +0900

    Append example with logger.

--- recipe/local_ruby_block.rb
+++ recipe/local_ruby_block.rb
@@ -10,3 +10,12 @@ local_ruby_block '' do
   end
 end
 
+# ref: https://github.com/itamae-kitchen/itamae/blob/0d548655aa9528bd1335ecb3b4a6880d7d6ad4ea/spec/integration/recipes/default.rb#L274-L278
+local_ruby_block 'call logger' do
+  block do
+    Itamae.logger.info "baz:info"
+    Itamae.logger.warn "baz:warn"
+    Itamae.logger.error "baz:error"
+    Itamae.logger.debug "baz:debug"
+  end
+end
