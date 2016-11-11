3ec7abf62217060bec3e818eacee46bd611794c2
Hidenori MATSUKI <MATSUKI.Hidenori@gmail.com>
2016-11-11 17:55:17 +0900

    Update recipe for notifies with `local_ruby_block`.

--- recipe/notifies.rb
+++ recipe/notifies.rb
@@ -1,7 +1,7 @@
 local_ruby_block 'foo' do
   action :nothing # required
   block do
-    Itamae.logger.info 'Foo!'
+    Itamae.logger.info 'Foo'
   end
 end
 
@@ -9,8 +9,21 @@ service "nginx" do
   action :nothing
 end
 
+# not effect
+Itamae.logger.info 'BEGIN:local_ruby_block'
+local_ruby_block '' do
+  block do
+    Itamae.logger.info 'Bar'
+  end
+  notifies :reload, "service[nginx]"
+  notifies :run, "local_ruby_block[foo]"
+end
+Itamae.logger.info 'END:local_ruby_block'
+
+Itamae.logger.info 'BEGIN:template'
 template "/etc/nginx/conf.d/app.conf" do
   source '../files/etc/nginx/conf.d/app.conf'
   notifies :reload, "service[nginx]"
   notifies :run, "local_ruby_block[foo]"
 end
+Itamae.logger.info 'END:template'
